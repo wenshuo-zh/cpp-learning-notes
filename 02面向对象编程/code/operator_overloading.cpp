3b93daf403fa686667914a48180b0f6ff26a6c3d
Dddz <1275324773@qq.com>
2026-07-22 00:33:43 +0800

    在复数类中进行重载运算符的实现（没写完）
    删除掉了默认的无参构造函数，添加了+=的运算符重载，弄清楚了可以链式调用（修改对象本身的值）的运算符要返回引用，打印函数添加了const关键字

--- 02面向对象编程/code/operator_overloading.cpp
+++ 02面向对象编程/code/operator_overloading.cpp
@@ -7,25 +7,34 @@ private:
 	int real;
 	int imag;
 public:
-	CComplex(){}
 	CComplex(int real):real(real),imag(0){}
 	CComplex(int real,int imag):real(real),imag(imag){}
 
-	void print() {
+	void const print() {
 		cout << real << "+" << imag << "i" << endl;
 	}
+
 	// +重载 成员函数
 	//CComplex operator+(const CComplex& );
-	friend CComplex operator+(const CComplex& , CComplex& );
+	friend CComplex operator+(const CComplex& , const CComplex& );
+
+	//+=重载
+	CComplex& operator+=(const CComplex& c) {     //支持链式调用，返回值要用引用（返回修改后的对象本身）
+		this->real = this->real + c.real;
+		this->imag = this->imag + c.imag;
+		return *this;
+	}
 };
+
 //CComplex CComplex::operator+(const CComplex& c) {
 //	return CComplex(this->real + c.real,this->imag + c.imag);
 //}
 
 //+重载 全局作用域下友元重载函数
-CComplex operator+(const CComplex& c1, CComplex& c2) {
+CComplex operator+(const CComplex& c1,  const CComplex& c2) {
 	return CComplex(c1.real + c2.real, c1.imag + c2.imag);
 }
+
 
 
 
@@ -38,4 +47,7 @@ int main() {
 	CComplex c4 = 10 + c2;
 	c3.print();
 	c4.print();
+
+	c3 += c1;
+	c3.print();
 }
